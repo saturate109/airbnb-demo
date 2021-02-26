198c52799cde51115bcca022d33bb4003972a37a
Senh Chang <mail@saturate.co.uk>
2021-02-26 00:30:25 +0000

    Remove popper from the AddDates componnet

--- src/components/AddDates.js
+++ src/components/AddDates.js
@@ -11,12 +11,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
 import Ratings from 'components/Ratings';
 import ReportListing from 'components/ReportListing';
 import AppContext from 'helpers/AppContext';
@@ -120,38 +114,15 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: 600,
     letterSpacing: '0.033rem',
   },
+  outlinedPrimary: {
+    '&:hover': {
+      borderBottom: 0,
+    },
+  },
 }));
-
-const options = [
-  'Create a merge commit',
-  'Squash and merge',
-  'Rebase and merge',
-];
 
 function AddDates() {
   const classes = useStyles();
-
-  const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef(null);
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
-
-  const handleMenuItemClick = (event, index) => {
-    setSelectedIndex(index);
-    setOpen(false);
-  };
-
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-
-    setOpen(false);
-  };
-
   const { dispatch } = useContext(AppContext);
 
   return (
@@ -183,6 +154,7 @@ function AddDates() {
                       root: classes.buttonCheckIn,
                       label: classes.buttonLabel,
                       outlinedSizeLarge: classes.outlinedSizeLarge,
+                      outlinedPrimary: classes.outlinedPrimary,
                     }}
                   >
                     <Typography
@@ -207,6 +179,7 @@ function AddDates() {
                       root: classes.buttonCheckOut,
                       label: classes.buttonLabel,
                       outlinedSizeLarge: classes.outlinedSizeLarge,
+                      outlinedPrimary: classes.outlinedPrimary,
                     }}
                   >
                     <Typography
@@ -257,7 +230,6 @@ function AddDates() {
                         />
                       </InView>
                     }
-                    onClick={handleToggle}
                   >
                     <Typography
                       display="block"
@@ -278,45 +250,6 @@ function AddDates() {
                     </Typography>
                   </Button>
                 </ButtonGroup>
-
-                <Popper
-                  open={open}
-                  anchorEl={anchorRef.current}
-                  role={undefined}
-                  transition
-                  disablePortal
-                >
-                  {({ TransitionProps, placement }) => (
-                    <Grow
-                      {...TransitionProps}
-                      style={{
-                        transformOrigin:
-                          placement === 'bottom'
-                            ? 'center top'
-                            : 'center bottom',
-                      }}
-                    >
-                      <Paper>
-                        <ClickAwayListener onClickAway={handleClose}>
-                          <MenuList id="split-button-menu">
-                            {options.map((option, index) => (
-                              <MenuItem
-                                key={option}
-                                disabled={index === 2}
-                                selected={index === selectedIndex}
-                                onClick={(event) =>
-                                  handleMenuItemClick(event, index)
-                                }
-                              >
-                                {option}
-                              </MenuItem>
-                            ))}
-                          </MenuList>
-                        </ClickAwayListener>
-                      </Paper>
-                    </Grow>
-                  )}
-                </Popper>
                 <CardActions classes={{ root: classes.cardActions }}>
                   <Button
                     variant="contained"
